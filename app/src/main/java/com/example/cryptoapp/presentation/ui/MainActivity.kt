eb5c35e9229a3a343e1eb4fbf77e9ac7f934682c
Sang <sang.nguyen2000@hotmail.de>
2022-01-25 13:31:29 +0100

    - dagger hilt set up

--- app/src/main/java/com/example/cryptoapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.cryptoapp.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cryptoapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/cryptoapp/presentation/ui/coinList/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/ui/coinList/CoinListViewModel.kt
@@ -1,13 +1,15 @@
 package com.example.cryptoapp.presentation.ui.coinList
 
 import androidx.lifecycle.ViewModel
-import com.example.cryptoapp.domain.useCase.getCoin.GetCoinUseCase
+import com.example.cryptoapp.domain.repository.CoinRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
 class CoinListViewModel @Inject constructor(
-    private val getCoinsUseCase: GetCoinUseCase
+    private val repository: CoinRepository,
+    private val dispatchers: Dispatchers
 ) : ViewModel() {
 
 
