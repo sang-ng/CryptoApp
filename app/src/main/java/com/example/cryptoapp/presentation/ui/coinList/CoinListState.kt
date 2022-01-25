3188079d78f2b642b1bd8a490013a5d3a176658f
Sang <sang.nguyen2000@hotmail.de>
2022-01-25 13:43:42 +0100

    - deleted unused files

--- app/src/main/java/com/example/cryptoapp/presentation/ui/coinList/CoinListState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cryptoapp.presentation.ui.coinList
-
-import com.example.cryptoapp.domain.model.Coin
-
-data class CoinListState(
-    val isLoading: Boolean = false,
-    val coins: List<Coin> = emptyList(),
-    val error: String = ""
-)
