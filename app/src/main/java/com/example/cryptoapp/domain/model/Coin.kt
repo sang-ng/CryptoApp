03e3e4ccded00e24feb61155704ce1b7a6dcafe2
Sang <sang.nguyen2000@hotmail.de>
2022-01-25 12:00:50 +0100

    implemented API and dto and models

---
+++ app/src/main/java/com/example/cryptoapp/domain/model/Coin.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoapp.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+//object we want to use for presentation
+data class Coin(
+    val id: String,
+    val isActive: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String
+)
